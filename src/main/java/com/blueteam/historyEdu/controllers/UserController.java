567ecb672583758509dd53ac0080cf885071a770
chinhbean09 <chinh0726@gmail.com>
2024-09-15 17:30:50 +0700

    generate secret key

--- src/main/java/com/blueteam/historyEdu/controllers/UserController.java
+++ src/main/java/com/blueteam/historyEdu/controllers/UserController.java
@@ -1,8 +1,22 @@
 package com.blueteam.historyEdu.controllers;
 
+import com.blueteam.historyEdu.components.JwtTokenUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("${api.prefix}/users")
+@RequiredArgsConstructor
 
 public class UserController {
+    private final JwtTokenUtils jwtTokenUtils;
+
+    @GetMapping("/generate-secret-key")
+    public ResponseEntity<?> generateSecretKey() {
+        return ResponseEntity.ok(jwtTokenUtils.generateSecretKey());
+    }
+
 }
